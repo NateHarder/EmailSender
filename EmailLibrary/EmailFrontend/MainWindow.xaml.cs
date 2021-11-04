40d4cc266e3f22add201594a0440297fd03b2348
NateHarder <hard5340@kettering.edu>
2021-11-03 21:21:47 -0700

    Added frontend, fatabase, and dll.

---
+++ EmailLibrary/EmailFrontend/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Windows;
+using EmailLibrary;
+
+namespace EmailFrontend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            EmailSender.Send(Recipient.Text, Subject.Text, Body.Text);
+        }
+    }
+}
---
+++ EmailLibrary/EmailLibrary/EmailSender.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data.SqlClient;
+using MimeKit;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+
+namespace EmailLibrary
+{
+    public class EmailSender
+    {
+
+
+        public static void Send(String recipient, String subject, String body) {
+
+            string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string path = (System.IO.Path.GetDirectoryName(executable));
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            var smtpClient = new MailKit.Net.Smtp.SmtpClient();
+            var message = new MimeMessage();
+            var mailMessage = new MimeMessage();
+            mailMessage.From.Add(new MailboxAddress(config["Smtp:Username"], config["Smtp:Email"]));
+            mailMessage.To.Add(new MailboxAddress("", recipient));
+            mailMessage.Subject = subject;
+            mailMessage.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+           
+            smtpClient.Connect(config["Smtp:Host"], Int32.Parse(config["Smtp:Port"]), true);
+            smtpClient.Authenticate(config["Smtp:Email"], config["Smtp:Password"]);
+
+            int sent = 1;
+            int attempts = 3;
+            while (sent == 1)
+            {
+                try
+                {
+                    smtpClient.Send(mailMessage);
+                    break;
+                }
+                catch
+                {
+                    if (--attempts == 0) 
+                    {
+                        sent = 0;
+                        throw; 
+                    }
+                    else Thread.Sleep(10000);
+                }
+            }
+
+            smtpClient.Disconnect(true);
+
+
+            using (SqlConnection connection = new SqlConnection(config["ConnectionStrings:EmailConnection"]))
+            {
+                string query = "INSERT INTO dbo.Emails (Sender,Recipient,Subject,Body,Date,Sent)"+
+                    " VALUES ('"+ config["Smtp:Email"] + "','"+ recipient + "','"+ mailMessage.Subject +"','"+ body +"','"+ DateTime.Now +"',"+sent+")";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
